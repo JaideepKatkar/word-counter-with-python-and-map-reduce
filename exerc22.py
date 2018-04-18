235e0d52fa6cef32b3f03a9e291b86bf0b7d62c1
Renato Rodrigues Martins <477935@sga.pucminas.br>
2018-04-17 22:05:17 -0300

    Improves the reduce function

--- exerc22.py
+++ exerc22.py
@@ -23,8 +23,16 @@ def mapfn(k, v):
         
 
 def reducefn(k, v):
-	print 'reduce ' + k
-	return v
+    print 'reduce ' + k
+    total = 0
+    for index, item in enumerate(v):
+        if item.split(":")[0] == 'Vendas':
+            total = int(item.split(":")[1]) +total
+        if item.split(":")[0] == 'Filial':
+            NomeFilial = item.split(":")[1]
+    L = list()
+    L.append(NomeFilial + " , " + str(total))
+    return L
 
 s = mincemeat.Server()
 
@@ -34,6 +42,6 @@ s.reducefn = reducefn
 
 results = s.run_server(password="changeme")
 
-w = csv.writer(open("C:\\Temp\\arq_exerc\\result2.csv", "w"))
+w = csv.writer(open("C:\\Temp\\arq_exerc\\result3.csv", "w"))
 for k, v in results.items():
-	w.writerow([k, v])
+	w.writerow([k, str(v).replace("[","").replace("]","").replace("'","").replace(' ','')])
